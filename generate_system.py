e7aa1331f21235d1a13cd2d0f0074acb8660705c
Philip Smith <ptsmith2808@gmail.com>
2021-10-15 23:56:54 +0100

    Added gravitational attraction between planets, this adds an n squared time scaling where n is the number of planets simulated. I have designed the algorithm to halve the number of calculations needed by only calculating the interaction eg n1 to n2 and reversing the sign for the interaction n2 to n1.

--- generate_system.py
+++ generate_system.py
@@ -11,17 +11,19 @@ class GenerateStockBody:
     """
     Generates our solar system
     """
-    def __init__(self, id):
-        obj = Horizons(id=id, location="@sun", epochs=Time("2018-01-01").jd, id_type='id').vectors()
+    def __init__(self, ID):
+        obj = Horizons(id=ID, location="@sun", epochs=Time("2018-01-01").jd, id_type='id').vectors()
         position = np.multiply([np.double(obj[xi]) for xi in ['x', 'y', 'z']], 149597870700) # gets the x,y,z position of earth and converts to si
         velocity = np.multiply([np.double(obj[vi]) for vi in ['vx', 'vy', 'vz']], (149597870700/(24*3600)))
-        self.id = id
+        mass_array = [3.3e23,4.87e24,5.97e24,6.43e23,1.898e27,5.68e26,8.68e25,1.02e26,1.46e22]
+        self.ID = ID
         self.position = np.array(position,dtype=np.float)
         self.velocity = np.array(velocity,dtype=np.float)
         self.xpoints = []
         self.ypoints = []
         self.name = np.str(obj['targetname'])
         self.time = 0
+        self.mass = mass_array[self.ID-1]
         self.orbital_radius = 0
 
 
@@ -29,15 +31,17 @@ class GenerateRandomBody:
     """
     Generates random solar system
     """
-    def __init__(self,id):
+    def __init__(self, ID):
         min_distance = 2e9
         max_distance = 1e12
         min_velocity = 0
         max_velocity = 5e4
         self.position = np.array([random.choice([-1,1])*random.randint(min_distance,max_distance),random.choice([-1,1])*random.randint(min_distance,max_distance),0], dtype=np.float)
         self.velocity = np.array([random.choice([-1,1])*random.randint(min_velocity,max_velocity),random.choice([-1,1])*random.randint(min_velocity,max_velocity),0], dtype=np.float)
+        self.acc = np.array([0, 0, 0])
         self.xpoints = []
         self.ypoints = []
-        self.id=id
+        self.ID = ID
         self.time = 0
-        self.orbital_radius=0
+        self.mass = random.randint(2e24,2e29)
+        self.orbital_radius = 0
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import time
+import cProfile, pstats, io
 from generate_system import *
-from simulate_orbits import SystemEvolve
 
 def main():
     start = time.time_ns()
@@ -15,22 +15,26 @@ class Simulation:
     """
     Holds the top level methods and parameters
     """
-    """Simulation parameters"""
     def __init__(self):
-        self.axes_limit = 2000e9
-        self.planet_count = 50  # planets to create, max 9 if using our solar system
-        self.plot_interval = 25  # in days
+        """Simulation parameters"""
+        self.random_planets = True  # if false use our solar system
+        self.planet_count = 25  # planets to create, max 9 if using our solar system
+        self.days_to_plot = 100  # the number of days for which the position of each planet is plotted
         self.dt = 3600 * 24  # in seconds
-        self.run_time = 30  # in years
-        self.planets = []
-        self.colours = ['gray', 'orange', 'blue', 'chocolate','brown','hotpink','black','green','violet']
-        self.mass = 1.989e30
-        self.dt = 3600*24
-        self.plot_data = True
-        self.random_planets = True
+        self.run_time = 25  # in years
+        self.sun_mass = 1.989e30
+        """Plotting parameters"""
+        self.plot_data = False  # choose whether to plot the positions of planets
+        self.axes_limit = 2000e9  # size of plot window in meters
+        self.plot_interval = 25  # in days
+        self.delete_distance = 30  # the distance from the Sun at which a planet is deleted in AU
+        self.colours = ['gray', 'orange', 'blue', 'chocolate','brown','hotpink','black','green','violet']  # line colours
+        """--------------------"""
+
         if not self.random_planets:
             self.planet_count = 9
 
+        self.planets = []
         self.create_planets()
 
         if self.plot_data:
@@ -40,13 +44,15 @@ class Simulation:
 
         else:
             for i in range(self.run_time * 365):
-                for planet in self.planets:
-                    self.evolve(planet)
+                self.evolve(self.planets)
+                if i % 25 == 0:
+                    print("Year:", self.planets[0].time / (self.dt * 365))
+                    print("ID:", self.planets[0].ID, "Position:", self.planets[0].position, "Velocity", self.planets[0].velocity, "Acceleration", self.planets[0].acc)
 
     def create_planets(self):
         for i in range(self.planet_count):
             """
-            Generates the planets and their associated plotting objects, planets are either generated with random velocity/positions or are based off our solar system
+            Generates the planets with random velocity/positions or based off our solar system
             """
             if self.random_planets:
                 self.planets.append(GenerateRandomBody(i + 1))
@@ -57,20 +63,54 @@ class Simulation:
     def create_plot(self):
         self.plot.create_plot(self)
 
-    def evolve(self, planet):
-        planet.position += planet.velocity * self.dt
-        planet.orbital_radius = np.linalg.norm(planet.position)
-        acc = -((const.G * self.mass * planet.position) / (planet.orbital_radius ** 3))
-        planet.velocity += acc * self.dt
-        planet.xpoints.append(planet.position[0])
-        planet.ypoints.append(planet.position[1])
-        planet.time += self.dt
-        if len(planet.xpoints) >= 100:
-            planet.xpoints.pop(0)
-            planet.ypoints.pop(0)
-        #if planet.id == 6: print(planet.position, planet.velocity, acc)
-        if planet.id == 6: print(planet.time/(self.dt*365))
+    def evolve(self, planets):
+        """
+        Updates the location of each planet based on its acceleration due to the Suns gravitational field only
+        """
+        length = len(planets)
+        # acc here stores a list of accelerations for each planet
+        acc = self.calc_planet_attraction(planets, length)
+        for n, planet in enumerate(planets):
+            planet.position += planet.velocity * self.dt
+            planet.orbital_radius = np.linalg.norm(planet.position)
+            planet.acc = -((const.G * self.sun_mass * planet.position) / (planet.orbital_radius ** 3))
+            planet.acc += self.collapse_acc(acc, planet, length, n)
+            planet.velocity += planet.acc * self.dt
+            planet.xpoints.append(planet.position[0])
+            planet.ypoints.append(planet.position[1])
+            planet.time += self.dt
+            if len(planet.xpoints) >= self.days_to_plot:
+                planet.xpoints.pop(0)
+                planet.ypoints.pop(0)
 
+    def calc_planet_attraction(self, planets, length):
+        """
+        Calculates the accelreration between each planet and every other planet, makes sure to not calculate the
+        attraction between eg p1 to p2 and between p2 to p1, instead using the first calculated value and changing
+        the sign.
+        """
+        acc = []
+        for n, planet_n in enumerate(planets):
+            for m, planet_m in enumerate(planets):
+                if n == m:
+                    acc.append(np.array([0,0,0]))
+                elif n>m:
+                    acc.append(-acc[m * length + n])
+                else:
+                    seperation = np.subtract(planet_n.position, planet_m.position)
+                    norm_seperation = np.linalg.norm(seperation)
+                    acc.append(np.array(-((const.G * planet_n.mass * seperation) / (norm_seperation ** 3))))
+        return acc
+
+    def collapse_acc(self, acc, planet, length, n):
+        """
+        Sums the component's of acc that apply to planet into a single acceleration which is added to the component
+        from the sun
+        """
+        acc_sum = np.array([0.0,0.0,0.0])
+        for m in range(length):
+            acc_sum += acc[length * n + m]
+        return acc_sum
 
 class Plotting:
     """
@@ -85,19 +125,21 @@ class Plotting:
         self.fig, (self.ax1, self.ax2) = plt.subplots(nrows=2, ncols=1)
         self.ax1.set_xlim(-simulation.axes_limit, simulation.axes_limit)
         self.ax1.set_ylim(-simulation.axes_limit, simulation.axes_limit)
+        self.timestamp = self.ax1.text(.03, .94, 'Day: ', color='b', transform=self.ax1.transAxes, fontsize='x-large')
+        self.sun = self.ax1.scatter([0], [0], color='yellow', s=100)
+
+        # Time plot variables
         self.times = []
         self.times.append(time.time_ns())
         self.time_steps = []
         self.x_time_data = []
         self.time_plot = self.ax2.plot(self.x_time_data, self.time_steps)
-        self.timestamp = self.ax1.text(.03, .94, 'Day: ', color='b', transform=self.ax1.transAxes, fontsize='x-large')
-        self.sun = self.ax1.scatter([0], [0], color='yellow', s=100)
 
     def create_plot(self, simulation):
         for i in range(simulation.planet_count):
             colour = simulation.colours[random.randint(0, 8)]
             plot = self.ax1.scatter([], [], color=colour, s=10)
-            label = self.ax1.annotate(simulation.planets[i].id, xy=(simulation.planets[i].position[:2]))
+            label = self.ax1.annotate(simulation.planets[i].ID, xy=(simulation.planets[i].position[:2]))
             line, = self.ax1.plot([], [], color=colour, linewidth=1)
             self.plots.append(plot)
             self.labels.append(label)
@@ -107,7 +149,7 @@ class Plotting:
         ani = animation.FuncAnimation(self.fig, self.animate, repeat=False, frames=self.frames,
                                       fargs=(simulation,), blit=True, interval=0, )
         if self.save_anim:
-            ani.save('C:/Users/Philip/Pictures/sim/animation' + str(time.time()) + '.gif', writer='imagemagick', fps=60)
+            ani.save('C:/Users/Philip/Pictures/sim/animation2' + str(time.time()) + '.gif', writer='imagemagick', fps=60)
         plt.show()
 
     def animate(self, i, simulation):
@@ -119,16 +161,23 @@ class Plotting:
         """
         self.delete_plots(simulation.planets)  # deletes old plots
         self.plot_time(i)  # creates the data for, and updates the time plot
-
         print("dt(frame) = ", self.time_steps[i], " ms")
-        year = simulation.planets[-1].time / (3600 * 24 * 365)
-
-        if year >= simulation.run_time:
+        if len(simulation.planets) > 0:
+           year = simulation.planets[-1].time / (3600 * 24 * 365)
+        else:
             plt.close()
 
+        if year >= simulation.run_time:
+            # ends the simulation
+            plt.close()
+        for count in range(simulation.plot_interval):
+            simulation.evolve(simulation.planets)
+
         for n, planet in enumerate(simulation.planets):
-            for count in range(simulation.plot_interval):
-                simulation.evolve(planet)
+            """ 
+            Calls the evolve method x times per animation frames where x=plot_interval and then updates all the plot
+            items. This is a lot quicker than updating the animation each time each planets position is updated.
+            """
             orbital_radius = planet.orbital_radius / 149597870700
             self.plots[n].set_offsets(planet.position[:2])
             self.lines[n].set_xdata(planet.xpoints)
@@ -163,4 +212,12 @@ class Plotting:
 
 
 if __name__ == "__main__":
+    # pr = cProfile.Profile()
+    # pr.enable()
     main()
+    # pr.disable()
+    # s= io.StringIO()
+    # sortby = 'cumulative'
+    # ps = pstats.Stats(pr,stream=s).sort_stats(sortby)
+    # ps.print_stats()
+    # print(s.getvalue())
